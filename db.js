526796f5e755a6d2b9875bfa2bc7028d912c308d
TimoElony <timo@elony.de>
2025-04-24 22:36:58 +0300

    changed supabase to connection pooling for IPv4 compatibility and changed pool settings

--- db.js
+++ db.js
@@ -2,11 +2,13 @@ const Pool = require('pg').Pool;
 require('dotenv').config();
 
 const pool = new Pool({
-    user: 'postgres',
-    host: 'db.vwpzcvemysspydbtlcxo.supabase.co',
+    user: 'postgres.vwpzcvemysspydbtlcxo',
+    host: 'aws-0-eu-central-1.pooler.supabase.com',
     database: 'postgres',
     port: 5432,
-    password: process.env.DB_PASSWORD
+    pool_mode: 'session',
+    password: process.env.DB_PASSWORD,
+    ssl: { rejectUnauthorized: false }
 
 });
 
