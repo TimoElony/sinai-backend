ed97dd57458dcdce43d26d24553bcd3b702db543
TimoElony <timo.elony@gmail.com>
2025-05-05 19:47:16 +0300

    added sector to new route submission

--- routes/climbingroutes.js
+++ routes/climbingroutes.js
@@ -46,7 +46,7 @@ router.post("/new", verifySupabaseToken, async (req,res) => {
     try {
         const {name, grade, bolts, length, info, area } = req.body;
         const newRoute = await pool.query(
-            "INSERT INTO climbing_routes (name, fa_grade, bolts, length, plain_description, climbing_area) VALUES ($1, $2, $3, $4, $5, $6) RETURNING name, id",
+            "INSERT INTO climbing_routes (name, fa_grade, bolts, length, plain_description, climbing_area, climbing_sector) VALUES ($1, $2, $3, $4, $5, $6) RETURNING name, id",
             [name, grade, bolts, length, info, area]
         );
 
