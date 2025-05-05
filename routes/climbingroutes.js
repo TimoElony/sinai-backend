93df07a6d3ec8172a37b71a2be38a78fc91c6d14
TimoElony <timo.elony@gmail.com>
2025-05-05 20:33:19 +0300

    order routes by descending editing date

--- routes/climbingroutes.js
+++ routes/climbingroutes.js
@@ -10,12 +10,12 @@ router.get("/:area/:crag", async (req,res) => {
         let allRoutes;
         if (crag === 'singlecrag') {
             allRoutes = await pool.query(
-                "SELECT id, name, grade_best_guess, length FROM climbing_routes WHERE climbing_area = $1 ORDER BY updated_at", 
+                "SELECT id, name, grade_best_guess, length FROM climbing_routes WHERE climbing_area = $1 ORDER BY updated_at DESC", 
                 [area]
             );
         } else {
         allRoutes = await pool.query(
-            "SELECT id, name, grade_best_guess, length FROM climbing_routes WHERE climbing_area = $1 AND climbing_sector = $2 ORDER BY updated_at", 
+            "SELECT id, name, grade_best_guess, length FROM climbing_routes WHERE climbing_area = $1 AND climbing_sector = $2 ORDER BY updated_at DESC", 
             [area, crag]
         );
         }
