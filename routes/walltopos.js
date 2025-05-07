81ecafcf4911b2fe94675c01d276ea5306ba9116
TimoElony <timo.elony@gmail.com>
2025-05-07 23:26:14 +0300

    added walltopo id because it is needed to compare

--- routes/walltopos.js
+++ routes/walltopos.js
@@ -5,7 +5,7 @@ const pool = require('../db');
 router.get('/:area/:crag', async (req,res) => {
     try {
         const { area, crag } = req.params;
-        const relevantTopos = await pool.query("SELECT name, description, details, extracted_filename, climbing_routes_ids, climbing_area_name, climbing_sector FROM wall_topos WHERE climbing_area_name = $1 AND climbing_sector = $2 ORDER BY updated_at",
+        const relevantTopos = await pool.query("SELECT id, name, description, details, extracted_filename, climbing_routes_ids, climbing_area_name, climbing_sector FROM wall_topos WHERE climbing_area_name = $1 AND climbing_sector = $2 ORDER BY updated_at",
             [area, crag]
         );
         res.json(relevantTopos.rows);
