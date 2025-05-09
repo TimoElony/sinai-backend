06f4093313224cc97db43e1b7ab5caaaf9642766
TimoElony <timo.elony@gmail.com>
2025-05-09 10:03:54 +0300

    allowing decoupling of route and topo as well through the update route

--- routes/climbingroutes.js
+++ routes/climbingroutes.js
@@ -59,10 +59,11 @@ router.post("/addToTopo", verifySupabaseToken, async(req,res) => {
 
 router.put("/updateTopoNumber", verifySupabaseToken, async(req,res) => {
     try {
-        const { id, wall_topo_numbers } = req.body;
+        const { id, wall_topo_ids, wall_topo_numbers } = req.body;
+
         const updatedRoute = await pool.query(
-            "UPDATE climbing_routes SET wall_topo_numbers = $1 WHERE id = $2;",
-            [wall_topo_numbers, id]
+            "UPDATE climbing_routes SET wall_topo_ids = $1 wall_topo_numbers = $2 WHERE id = $3;",
+            [wall_topo_ids, wall_topo_numbers, id]
         )
         res.json(updatedRoute.rows);
     } catch (error) {
