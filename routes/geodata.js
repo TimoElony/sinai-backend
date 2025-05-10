7924de710724d6447ac98fefca651646deb33b0c
TimoElony <timo.elony@gmail.com>
2025-05-10 22:25:23 +0300

    giving more info for topo points

--- routes/geodata.js
+++ routes/geodata.js
@@ -4,7 +4,7 @@ const pool = require('../db');
 
 router.get('/topos', async (req,res) => {
     try {
-        const response = await pool.query("SELECT description, longitude, latitude FROM wall_topos LIMIT 150");
+        const response = await pool.query("SELECT description, longitude, latitude, climbing_area_name, climbing_sector FROM wall_topos LIMIT 150");
         res.json(response.rows);
     } catch (error) {
         console.error('couldnt query locations of walltopos')
