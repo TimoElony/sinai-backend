96e35d479e220b2b13312722b4960be0a0970c7a
TimoElony <timo.elony@gmail.com>
2025-11-12 14:01:56 +0100

    removed userId sources entry in routes table

--- routes/climbingroutes.js
+++ routes/climbingroutes.js
@@ -30,13 +30,13 @@ router.post("/new", verifySupabaseToken, async (req,res) => {
     try {
         const {name, grade, bolts, length, info, area, crag, setters } = req.body;
         const newRoute = await pool.query(
-            "INSERT INTO climbing_routes (name, fa_grade, bolts, length, plain_description, climbing_area, climbing_sector, setters, grade_best_guess, sources) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10) RETURNING name, id",
-            [name, grade, bolts, length, info, area, crag, setters, grade, req.user.id]
+            "INSERT INTO climbing_routes (name, fa_grade, bolts, length, plain_description, climbing_area, climbing_sector, setters, grade_best_guess) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING name, id",
+            [name, grade, bolts, length, info, area, crag, setters, grade]
         );
-        // const logEntry = await pool.query(
-        //     "INSERT INTO change_logs (user_id, action) VALUES ($1, $2)",
-        //     [req.user.email, 'Created new route'+name]
-        // );
+        const logEntry = await pool.query(
+            "INSERT INTO change_logs (user_id, action) VALUES ($1, $2)",
+            [req.user.email, 'Created new route'+name]
+        );
         res.json(newRoute.rows);
     } catch (error) {
         console.error('error adding new route', error.message);
